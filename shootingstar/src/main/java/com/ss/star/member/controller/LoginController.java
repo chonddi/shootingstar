e07ab2fe9e40b7c4a65d56c9a581bad53bb1be2d
hkedu <hkedu@192.168.2.104>
2018-07-20 17:53:20 +0900

    top이랑 로그인, favicon

--- shootingstar/src/main/java/com/ss/star/member/controller/LoginController.java
+++ shootingstar/src/main/java/com/ss/star/member/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.ss.star.member.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +31,7 @@ public class LoginController {
 	public String login_member(@RequestParam String memberId, @RequestParam String pwd, @RequestParam(required=false) String saveId,
 			HttpServletRequest request, HttpServletResponse response, Model model) {
 		logger.info("고객회원 로그인 memberId: {}, pwd: {}", memberId, pwd);
+		logger.info("saveId:{}", saveId);
 
 		int result = memberService.checkPwd(memberId, pwd);
 		logger.info("로그인 처리 결과 result: {}", result);
@@ -38,7 +40,7 @@ public class LoginController {
 		String msg="", url="/login/login.do";
 
 
-		if(result==memberService.LOGIN_OK) {
+		if(result==MemberService.LOGIN_OK) {
 			String name= memberService.selectNameById(memberId);
 			logger.info("로그인 처리 memberId로  불러온 결과 name: {}",name);
 			//[1] 세션
@@ -60,9 +62,9 @@ public class LoginController {
 
 			msg=name+"님 로그인되었습니다.";
 			url="/index.do";
-		}else if(result == memberService.PWD_DISAGREE) {
+		}else if(result == MemberService.PWD_DISAGREE) {
 			msg="비밀번호가 일치하지 않습니다.";
-		}else if(result == memberService.ID_NONE) {
+		}else if(result == MemberService.ID_NONE) {
 			msg="아이디가 없습니다";
 		}else {
 			msg="로그인 처리 실패";
@@ -79,7 +81,8 @@ public class LoginController {
 	public String login_sMember(@RequestParam String sMemberId, @RequestParam String sPwd, @RequestParam(required=false) String sSaveId,
 			HttpServletRequest request, HttpServletResponse response, Model model) {
 		logger.info("전문가회원 로그인 sMemberId: {}, sPpwd: {}", sMemberId, sPwd);
-
+		logger.info("sSaveId:{}", sSaveId);
+		
 		int result = memberService.checkPwd(sMemberId, sPwd);
 		logger.info("로그인 처리 결과 result: {}", result);
 
@@ -87,7 +90,7 @@ public class LoginController {
 		String msg="", url="/login/login.do";
 
 
-		if(result==memberService.LOGIN_OK) {
+		if(result==MemberService.LOGIN_OK) {
 			String sName= memberService.selectNameById(sMemberId);
 			logger.info("로그인 처리 sMemberId로  불러온 결과 sName: {}",sName);
 			//[1] 세션
@@ -109,9 +112,9 @@ public class LoginController {
 
 			msg=sName+"님 로그인되었습니다.";
 			url="/index.do";
-		}else if(result == memberService.PWD_DISAGREE) {
+		}else if(result == MemberService.PWD_DISAGREE) {
 			msg="비밀번호가 일치하지 않습니다.";
-		}else if(result == memberService.ID_NONE) {
+		}else if(result == MemberService.ID_NONE) {
 			msg="아이디가 없습니다";
 		}else {
 			msg="로그인 처리 실패";
@@ -123,4 +126,23 @@ public class LoginController {
 		return "common/message";
 
 	}
+	
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
+		int userCode = (int)session.getAttribute("userCode");
+		logger.info("로그아웃 처리 userCode: {}", userCode);
+		
+		if(userCode==1) {
+			session.removeAttribute("memberId");
+			session.removeAttribute("userCode");
+			session.removeAttribute("name");
+		}else if(userCode==2) {
+			session.removeAttribute("sMemberId");
+			session.removeAttribute("userCode");
+			session.removeAttribute("sName");
+		}
+		
+		return "redirect:/index.do";
+	}
+	
 }
